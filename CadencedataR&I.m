d9bc025ea9bba9a9b2b493b986e540a4de8c9d89
ramelluru <103531813+ramelluru@users.noreply.github.com>
2023-02-23 12:28:51 -0500

    Update CadencedataR&I.m

--- CadencedataR&I.m
+++ CadencedataR&I.m
@@ -1,27 +1,30 @@
-   for i=1:10
-         b=book(i,2);
-         %disp(b);
-         if  b=="Mayfield_:IIP3_dBm_-22in_7125M"
+ [rows, columns]=size(book);
+ for i=1:rows
+         b=book(i,3);
+         if b=="IIP3_dBm_-22in_7125M"
             m=book(i-1,1);
-            num = sscanf(m, "Parameters: R1=%d X=%d");
-            disp('Real number');
-            disp(num(1));
-            disp('Real number');
-            disp(num(2));
+            num = zeros(1, 2);
+            [numbr, matches] = sscanf(m, "Parameters: R1=%d X=%d");
+            if matches == 1
+            num(1) = numbr(1);
+            elseif matches == 2
+            num(1:2) = numbr(1:2);
+            end
             IIP3=str2double(book(i,7));
             OIP3=str2double(book(i+1,7));
-            disp('IIP3:');
-            disp(IIP3);
-            disp('OIP3:'); 
-            disp(OIP3);
             gain=OIP3-IIP3;
-            disp('gain');
             disp(gain);
             im=num(2)*1i*(1:1:1);
             datr=num(1)+im;
             if gain>15
-            z0 = 50;
+            z0 = 50;    
             gamma1 = z2gamma(datr,z0);
+            if gamma1 == 0
+                q=num(1)+(1*10^-14);
+                datr=q+im;
+                gamma1 = z2gamma(datr,z0);
+                disp(gamma1);
+            end
             smithplot(gamma1,".",'Color','g');
             hold on;
             elseif (gain>14)&&(gain<15)
@@ -36,4 +39,4 @@
             hold on;
             end
         end
-   end
+end
